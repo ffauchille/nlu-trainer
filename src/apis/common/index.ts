1daecb9476b2abdffd765d7025a83a3353ff397e
ffauchille <florent.fauchille@kpmg.lu>
2018-09-10 16:17:57 +0200

    default env as nlu trainer api

--- src/apis/common/index.ts
+++ src/apis/common/index.ts
@@ -4,11 +4,9 @@ import { Observable } from 'rxjs/internal/Observable';
 
 const headers = { "Content-Type": "application/json" };
 
-const host = process.env.NLU_TRAINER_API
-
 console.log("host is ", process.env.NLU_TRAINER_API);
 
-const withHost = (route: string) => host + (route.startsWith("/") ? route : "/" + route)
+const withHost = (route: string) => process.env.NLU_TRAINER_API + (route.startsWith("/") ? route : "/" + route)
 
 export function get<T>(route: string): Observable<T> { return ajax.getJSON<T>(withHost(route), headers) }
 export function post<B, T>(route: string, body: B): Observable<T> { 
